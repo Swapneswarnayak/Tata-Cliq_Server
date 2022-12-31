d399a98d298a5eb8ddd54c06280c58ba8e759185
Swapneswarnayak <1705835@kiit.ac.in>
2023-01-01 02:13:27 +0530

    Added

--- all_Routes/usersRoutes.js
+++ all_Routes/usersRoutes.js
@@ -17,7 +17,7 @@ route.post('/register', async (req,res)=>{
    let body=req.body;
     try {
         const data=await Users.registeredNewUser(body);
-        return res.status(200).send(data);
+        return res.status(200).send({data});
     } catch (error) {
         return res.status(500).send(error.message);
         
